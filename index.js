d7754021ba39d933bfdb4c9c976aff2977b198ac
E-SPIKE <1an.3dwards1234@gmail.com>
2023-08-17 18:23:23 +1000

    layout now made in javascript

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 $(document).ready(function(){
-    loadscreen();
+    
+    createScreenLayout();
+
+    createTabs();
+
+    createMainScreen();
+
+    //loadscreen();
 });
+
+
+function createScreenLayout() {
+
+    this.outerBody = $(document.createElement('div')).addClass('outer-body').appendTo('body');
+
+    this.tabs = $(document.createElement('div')).addClass('tabs').appendTo(this.outerBody);
+
+    this.mainScreen = $(document.createElement('div')).addClass('main-screen').appendTo(this.outerBody);
+
+    this.messages = $(document.createElement('div')).addClass('messages').appendTo(this.outerBody);
+
+}
+
+function createTabs() {
+
+    this.tabMainBtn = $(document.createElement('button')).text('main').appendTo(this.tabs);
+
+    this.tabMapBtn = $(document.createElement('button')).text('map').appendTo(this.tabs);
+
+    this.tabAtlasBtn = $(document.createElement('button')).text('atlas').appendTo(this.tabs);
+}
+
+function createMainScreen() {
+
+    
+
+}
+
+
+
 
 
 
 function loadscreen() {
-    hello = $(document.createElement('button')).text('hello').appendTo('body');
+    this.hello = $(document.createElement('button')).text('hello').appendTo(this.outerBody);
     
     hello.on('click', function() {
-        alert('hello');
+        alert('hello world');
     })
 }
